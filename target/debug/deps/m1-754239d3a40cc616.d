0bb75e1ba0b403ecc5c0fe0bc8a9a484fc8f48fd
jeonghoson5 <jeongho.son5@gmail.com>
2022-01-12 18:29:05 +0900

    fix3

--- target/debug/deps/m1-754239d3a40cc616.d
+++
@@ -1,6 +0,0 @@
-/Users/son/Documents/Workspace/cargo-module-test/target/debug/deps/m1-754239d3a40cc616.rmeta: m1/src/lib.rs m1/src/module1.rs
-
-/Users/son/Documents/Workspace/cargo-module-test/target/debug/deps/m1-754239d3a40cc616.d: m1/src/lib.rs m1/src/module1.rs
-
-m1/src/lib.rs:
-m1/src/module1.rs:
